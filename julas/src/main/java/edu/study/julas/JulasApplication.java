91ac619405a5d0d807f99e2c80f8178da76c8478
Joonsung Kang <kuki19182000@gmail.com>
2022-01-31 21:47:24 +0900

    220131 first commit

---
+++ julas/src/main/java/edu/study/julas/JulasApplication.java
@@ -0,0 +1,13 @@
+package edu.study.julas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulasApplication.class, args);
+	}
+
+}
---
+++ julas/src/test/java/edu/study/julas/JulasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.study.julas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
